80244ea1865de9fcdd3a28778b94032f95902295
Johnnie Walker <rockwave.fm@gmail.com>
2020-09-30 12:12:27 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Johnnie Walker on 9/30/20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
