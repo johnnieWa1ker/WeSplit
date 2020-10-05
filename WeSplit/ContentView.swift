84774bc2311fce93e642d667fc3e116204ef1501
Johnnie Walker <rockwave.fm@gmail.com>
2020-10-05 11:55:20 +0300

    Add base features

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var amount: String = ""
+    @State private var numberOfPeople: Int = 2
+    @State private var tipPercentage: Int = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(amount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $amount)
+                        .keyboardType(.decimalPad)
+                    Picker("Count of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section(header: Text("How much tip would you like to tip?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+            .navigationTitle("WeSplit")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewLayout(.device)
+            .previewDevice("iPhone 11 Pro")
     }
 }
---
+++ WeSplit/README.md
@@ -0,0 +1,23 @@
+# 🤑 WeSplit
+[![Build Status](https://img.shields.io/badge/Swift-5.3-orange.svg)](https://swift.org)
+
+A test project in which I am learning the SwiftUI framework.
+The project is part of Paul Hudson's training cycle - 100 Days of SwiftUI
+
+  <p float="right">
+  <img src="WeSplit/WeSplit/Assets.xcassets/demo-1.imageset/demo-1.png" width="32%"/>
+  </p>
+  
+
+## 🏃‍♂️ Getting Started
+
+``` bash
+git clone https://github.com/johnnieWa1ker/WeSplit.git
+cd WeSplit
+pod install
+open WeSplit.xcodeproj.
+```
+
+## 🎉 Why am I building this?
+1. I am very interested in the SwiftUI framework from Apple, I believe that after a while it will become quite powerful so that you can quickly make cool projects with it
+1. I want to master the 100 Days SwiftUI marathon 🏃‍♂️
